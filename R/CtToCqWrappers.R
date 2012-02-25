9c5542adf5d20d4aed2d891a9405b7ef0270554c
Matthias Kohl <Matthias.Kohl@stamats.de>
2012-02-25 23:05:36 +0000

    minor corrections to deltaCq and deltaDeltaCq, increased version number to 1.1.2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/NormqPCR@63162 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/CtToCqWrappers.R
+++ R/CtToCqWrappers.R
@@ -1,7 +1,10 @@
-deltaDeltaCq <- function(qPCRBatch, maxNACase, maxNAControl, hkgs, contrastM, case, control, paired, hkgCalc, statCalc){
-  deltaDeltaCt(qPCRBatch, maxNACase=0, maxNAControl=0, hkgs, contrastM, case, control, paired=TRUE, hkgCalc="arith", statCalc="arith")
+deltaDeltaCq <- function(qPCRBatch, maxNACase=0, maxNAControl=0, hkgs, contrastM, case, control, 
+                         paired=TRUE, hkgCalc="arith", statCalc="arith"){
+  deltaDeltaCt(qPCRBatch=qPCRBatch, maxNACase=maxNACase, maxNAControl=maxNAControl, hkgs=hkgs, 
+               contrastM=contrastM, case=case, control=control, paired=paired, hkgCalc=hkgCalc, 
+               statCalc=statCalc)
 }
 
-deltaCq <- function(qPCRBatch, hkgs, combineHkgs, calc){
-  deltaCt(qPCRBatch, hkgs, combineHkgs=FALSE, calc="arith")
+deltaCq <- function(qPCRBatch, hkgs, combineHkgs=FALSE, calc="arith"){
+  deltaCt(qPCRBatch=qPCRBatch, hkgs=hkgs, combineHkgs=combineHkgs, calc=calc)
 }
